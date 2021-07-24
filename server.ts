0cd9c693006bb85a7aca13bf2844a93f1a60865a
kelbri10 <kelbri10@gmail.com>
2021-07-24 18:29:34 -0400

    adds user, profile, post controllers to controllers folder

--- server.ts
+++ server.ts
@@ -1,10 +1,14 @@
 const express = require('express'); 
 const app = express(); 
-const PORT: number = 3000; 
+const PORT: string | number = process.env.PORT || 3000; 
 
-app.get('/', (req, res) => { 
+app.get('/', (req: Request, res: Response) => { 
     res.send('Express server is live'); 
 })
+
+//middleware 
+app.use(express.json()); 
+
 
 app.listen(PORT, () => { 
     console.log( `App is listening on PORT ${PORT}`); 
