c897dcd30d85e6cade25d52e22b1c3bd9dbacc35
kelbri10 <kelbri10@gmail.com>
2021-07-25 18:52:48 -0400

    imports box to use flexbox direction on textfields

--- client/src/components/CreateAccount.tsx
+++ client/src/components/CreateAccount.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from "react";
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import { Button, TextField, Select} from "@material-ui/core";
-
+import Box from '@material-ui/core/Box'; 
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles ({
@@ -26,58 +26,62 @@ const CreateAccount: FC = () => {
     const classes = useStyles(); 
 
     return ( 
-        <div>
+        <div style={{ width: '100%'}}>
             <h1>Connected</h1>
 
             <h2>It only takes a minute to get connected!</h2>
 
-            <form className={classes.root} noValidate autoComplete='off'>
-
-                <div>
-                    <TextField 
+            <Box display='flex' flexDirection='column' p={1} m={1}>
+                <form className={classes.root} noValidate autoComplete='off'>
+                    <Box p={0.5}>
+                        <TextField 
                         id='username' 
                         label='Username'
-                
                         required 
-                    />
-                </div>
-                <div>
-                    <TextField 
+                        />
+                    </Box>
+
+                    <Box p={0.5}>
+                        <TextField 
                         id='password' 
                         label='Password' 
-                    
-                        required
-                    />
-                </div>
-                <div>
-                    <TextField 
-                        id='displayName' 
-                        label='Display Name' 
-                        
-                        required
-                    />
-                </div>
-                <div>
-                    <TextField 
-                        id='email' 
-                        label='Email' 
-                    
                         required
                         />
-                </div>
-                <div>
-                    <TextField 
+                    </Box>
+
+                    <Box p={0.5}>
+                        <TextField 
+                        id='displayName' 
+                        label='Display Name' 
+
+                        required
+                        />
+                    </Box>
+
+                    <Box p={0.5}> 
+                        <TextField 
+                        id='email' 
+                        label='Email' 
+
+                        required
+                        />
+                    </Box>
+
+                    <Box p={0.5}>
+                        <TextField 
                         id='location' 
                         label='Location' 
-                    
-                    />
-                </div>
-
-                <div>
+                        />
+                    </Box>
+            
                     <Button>Submit</Button>
-                </div>
+            
 
-            </form>
+                </form>
+        
+            </Box>
+
+            
 
             <p>Already have an account? Log in </p>
         </div>
