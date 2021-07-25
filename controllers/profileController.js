fa7aad0262fc47b6a72495721b1d6c120feba7e3
kelbri10 <kelbri10@gmail.com>
2021-07-25 01:09:08 -0400

    adds dist folder with compiled js

--- controllers/profileController.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-//import UserProfile from '../model/user.model'; 
-// POST /user - create a new user 
-//api/user 
-// GET /user/:userID - gets specific user profile based on authetication
-//api/user/
