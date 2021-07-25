ea64d5291bb4a4c6b021479267fb71d42b287210
kelbri10 <kelbri10@gmail.com>
2021-07-25 18:38:12 -0400

    adds imports to Profile component

---
+++ client/src/components/Profile.tsx
@@ -0,0 +1,6 @@
+import React, { FC } from 'react';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import { Button } from '@material-ui/core'; 
+import Paper from '@material-ui/core/Paper'; 
+
+
