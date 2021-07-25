9e7a0b3a8964dc15e664863db638d9b93ee581e1
kelbri10 <kelbri10@gmail.com>
2021-07-25 01:18:59 -0400

    removes duplicate js files

--- routes/api-routes.ts
+++ routes/api-routes.ts
@@ -4,7 +4,7 @@ import UserProfile from '../model/user.model';
 const router = express.Router(); 
 
 // POST method for adding new users to app
-router.post('/api/user', (req, res) =>{ 
+router.post('/api/user', (req: Request, res: Response) =>{ 
     const newUser = UserProfile.create({
         username: req.body.username, 
         name: req.body.name, 
