7b1a4a724a8b8cdc47870b3fd39731045fd71276
kelbri10 <kelbri10@gmail.com>
2021-07-25 20:18:36 -0400

    adds table container and table headers to profiletable.tsx

--- client/src/components/ProfileTable.tsx
+++ client/src/components/ProfileTable.tsx
@@ -5,6 +5,25 @@ const createProfilePage = (albums: {count: number, categories: string[]}, connec
     return {albums, connections, feed}; 
 }
 
-const row = createProfilePage({count: 3, categories: ['style', 'makeup', 'nature']}, {count: 2, users: ['max', 'peyton']}, {count: 2, posts: ['text', 'picture']}); 
+const row = createProfilePage(
+    {count: 3, categories: ['style', 'makeup', 'nature']}, 
+    {count: 2, users: ['max', 'peyton']}, 
+    {count: 2, posts: ['text', 'picture']}); 
+
+const ProfileTable: FC = () => { 
+    return( 
+        <TableContainer>
+            <Table>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Albums</TableCell>
+                        <TableCell>Connections</TableCell>
+                        <TableCell>Feed</TableCell>
+                    </TableRow>
+                </TableHead>
+            </Table>
+        </TableContainer>
+    )
+} 
 
 
